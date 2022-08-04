72315473d29c5cf509c55823bea91fcf04360b21
Roy Palomino <roy100995@gmail.com>
2022-08-04 13:31:20 -0500

    feat: add form and changes for state

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,43 +1,120 @@
 import 'package:flutter/material.dart';
 
-const _appName = "FDA";
-
 class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+  const Login({super.key});
 
   @override
   State<Login> createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final String _appName = "FDA";
+  bool _showPassword = true;
+  void _togglePassword() {
+    _showPassword = !_showPassword;
+    print(_showPassword);
+  }
+
+  void _goToRegister() {
+    print("route to register page!!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        Divider(height: 25.0),
-        Image(image: NetworkImage("https://i.stack.imgur.com/yZlqh.png")),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Divider(height: 50.0),
+            const FittedBox(
+              fit: BoxFit.fill,
+              child: Image(
+                image: NetworkImage("https://i.stack.imgur.com/yZlqh.png"),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 25.0, horizontal: 20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Bienvenido a $_appName",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w700, fontSize: 20.0),
+                    ),
 
-        // Bienvenido a XYZ
-        Text(
-          "Bienvenido a $_appName",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20.0),
+                    // Inicie sesión o cree una cuenta
+                    Row(
+                      children: [
+                        const Text("Inicia sesión o"),
+                        TextButton(
+                          onPressed: _goToRegister,
+                          child: const Text(
+                            "cree una cuenta",
+                            style:
+                                TextStyle(decoration: TextDecoration.underline),
+                          ),
+                        )
+                      ],
+                    ),
+                    TextFormField(
+                      decoration:
+                          const InputDecoration(hintText: "Correo electrónico"),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter some text";
+                        }
+                        return null;
+                      },
+                    ),
+
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Contraseña",
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _togglePassword();
+                            });
+                          },
+                          child: Icon(_showPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                        ),
+                      ),
+                      obscureText: !_showPassword,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter some text";
+                        }
+                        return null;
+                      },
+                    ),
+
+                    Text(_showPassword.toString()),
+
+                    // input mail
+
+                    // input passwd
+
+                    // forgot passwd? | Iniciar sesión
+
+                    // label: Inicie sesión con G+ facebook appel
+
+                    // link: continuar como invitado
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-
-        // Inicie sesión o cree una cuenta
-
-        // input mail
-
-        // input passwd
-
-        // forgot passwd? | Iniciar sesión
-
-        // label: Inicie sesión con G+ facebook appel
-
-        // link: continuar como invitado
-      ],
-    ));
+      ),
+    );
   }
 }
