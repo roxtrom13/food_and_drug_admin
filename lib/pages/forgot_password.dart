bb22fa9df7b9c02a1079ed99681e8e3d696a850d
Roy Palomino <roy100995@gmail.com>
2022-08-05 19:34:31 -0500

    feat: create page to restore password

---
+++ lib/pages/forgot_password.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _email = "";
+
+  void sendRestoreEmail() {
+    print("$_email");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text("¿Olvidaste tu contraseña?"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 40.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const Text(
+                "Ingresa el correo electrónico asociado a la cuenta y te enviaremos un código de verificación",
+                style: TextStyle(fontSize: 16.0),
+              ),
+              TextFormField(
+                onChanged: (value) {
+                  _email = value;
+                },
+                decoration: const InputDecoration(
+                  hintText: "Ingresa tu correo electrónico",
+                ),
+                validator: (value) {
+                  if (value!.contains(RegExp(
+                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"))) {
+                    return "Ingrese un correo válido";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30.0),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  fixedSize: const Size(double.infinity, 50),
+                ),
+                child: const Text("Enviar link"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
