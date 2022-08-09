51f4e36f7a5bd8ab4df5e1e23ec21e0edb063a10
Roy Palomino <roy100995@gmail.com>
2022-08-09 12:57:22 -0500

    refactor: complete profile view basics structure without style

--- lib/pages/complete_profile.dart
+++ lib/pages/complete_profile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:country_picker/country_picker.dart';
+
+import '../assets/constants.dart' as constants;
 
 class CompleteProfile extends StatefulWidget {
   const CompleteProfile({Key? key}) : super(key: key);
@@ -13,10 +16,13 @@ class _CompleteProfileState extends State<CompleteProfile> {
   String _firstName = "";
   String _lastName = "";
   DateTime _birthDate = DateTime.now();
-  num _sex = 1;
   String _country = "";
+  constants.Gender? _gender = constants.Gender.female;
+
   static const _birthDayStringBase = "Ingresa tu fecha de nacimiento";
   String _birthDayString = "Ingresa tu fecha de nacimiento";
+  static const _selectCountryStringBase = "País";
+  String _selectCountryString = "País";
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +33,22 @@ class _CompleteProfileState extends State<CompleteProfile> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                const Text("Completa tu perfil"),
+                const Text(
+                  "Completa tu perfil",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
                 const Icon(
                   Icons.account_circle,
                   size: 80.0,
                 ),
+                const SizedBox(height: 20.0),
                 const Text(
                     "Sube una foto para poder identificarte más fácilmente"),
+                const SizedBox(height: 20.0),
                 Form(
                   key: _formKey,
                   child: Column(
@@ -45,7 +60,14 @@ class _CompleteProfileState extends State<CompleteProfile> {
                         decoration: const InputDecoration(
                           hintText: "Ingresa tus nombres",
                         ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Este campo es requerido";
+                          }
+                          return null;
+                        },
                       ),
+                      const SizedBox(height: 15.0),
                       TextFormField(
                         onChanged: (value) {
                           _lastName = value;
@@ -53,7 +75,14 @@ class _CompleteProfileState extends State<CompleteProfile> {
                         decoration: const InputDecoration(
                           hintText: "Ingresa tus apellidos",
                         ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Este campo es requerido";
+                          }
+                          return null;
+                        },
                       ),
+                      const SizedBox(height: 15.0),
                       Padding(
                         padding: const EdgeInsets.only(top: 12.0),
                         child: GestureDetector(
@@ -78,9 +107,10 @@ class _CompleteProfileState extends State<CompleteProfile> {
                               Text(
                                 _birthDayString,
                                 style: TextStyle(
-                                  fontSize: 15,
+                                  fontSize: 15.9,
+                                  fontWeight: FontWeight.w400,
                                   color: _birthDayString == _birthDayStringBase
-                                      ? Colors.grey.shade600
+                                      ? Colors.grey.shade700
                                       : Colors.black,
                                 ),
                               ),
@@ -97,6 +127,117 @@ class _CompleteProfileState extends State<CompleteProfile> {
                         thickness: 1,
                         endIndent: 0,
                         color: Colors.grey,
+                      ),
+                      const SizedBox(height: 15.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: 98,
+                            child: RadioListTile(
+                              visualDensity: VisualDensity.compact,
+                              title: const Text("Mujer"),
+                              contentPadding: const EdgeInsets.all(0),
+                              value: constants.Gender.female,
+                              groupValue: _gender,
+                              onChanged: (constants.Gender? value) {
+                                setState(() {
+                                  _gender = value;
+                                });
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: 98,
+                            child: RadioListTile(
+                              visualDensity: VisualDensity.compact,
+                              contentPadding: const EdgeInsets.all(0),
+                              title: const Text("Varón"),
+                              value: constants.Gender.male,
+                              groupValue: _gender,
+                              onChanged: (constants.Gender? value) {
+                                setState(() {
+                                  _gender = value;
+                                });
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: 98,
+                            child: RadioListTile(
+                              visualDensity: VisualDensity.compact,
+                              contentPadding: const EdgeInsets.all(0),
+                              title: const Text("Otro"),
+                              value: constants.Gender.other,
+                              groupValue: _gender,
+                              onChanged: (constants.Gender? value) {
+                                setState(() {
+                                  _gender = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15.0),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            showCountryPicker(
+                              context: context,
+                              favorite: ['PE'],
+                              onSelect: (Country country) {
+                                setState(() {
+                                  _country = country.name;
+                                  _selectCountryString = country.name;
+                                });
+                              },
+                            );
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                _selectCountryString,
+                                style: TextStyle(
+                                  fontSize: 15.9,
+                                  fontWeight: FontWeight.w400,
+                                  color: _selectCountryString ==
+                                          _selectCountryStringBase
+                                      ? Colors.grey.shade700
+                                      : Colors.black,
+                                ),
+                              ),
+                              const Icon(
+                                Icons.search,
+                                color: Colors.blue,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Divider(
+                        height: 20,
+                        thickness: 1,
+                        endIndent: 0,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(height: 20.0),
+                      FractionallySizedBox(
+                        widthFactor: 1,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              // save user data
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                            fixedSize: const Size(0, 50),
+                          ),
+                          child: const Text("Completar perfil"),
+                        ),
                       ),
                     ],
                   ),
