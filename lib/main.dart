6b8724cbd382f7a8de24cee8ea0b84b4119533b4
Roy Palomino <roy100995@gmail.com>
2022-08-09 12:56:29 -0500

    feat: setup localizations delegates por country picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:food_and_drug_admin/pages/complete_profile.dart';
 import 'package:food_and_drug_admin/pages/forgot_password.dart';
 import 'package:food_and_drug_admin/pages/login.dart';
 import 'package:food_and_drug_admin/pages/register.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +18,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Food and Drug Administrator',
       debugShowCheckedModeBanner: false,
+      supportedLocales: const [
+        Locale("es"),
+      ],
+      localizationsDelegates: const [
+        CountryLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
         backgroundColor: const Color(0xfffb923c),
