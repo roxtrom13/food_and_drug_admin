ea00028ebb9923006a8d5758122bf12aa86e0089
Roy Palomino <roy100995@gmail.com>
2022-08-11 13:10:01 -0500

    reorder imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:food_and_drug_admin/pages/complete_profile.dart';
 import 'package:food_and_drug_admin/pages/forgot_password.dart';
 import 'package:food_and_drug_admin/pages/login.dart';
 import 'package:food_and_drug_admin/pages/register.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:food_and_drug_admin/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
