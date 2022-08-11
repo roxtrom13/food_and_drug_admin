6559e3acde9c32a704c0742549c4d01e0d5c9d3d
Roy Palomino <roy100995@gmail.com>
2022-08-11 11:46:26 -0500

    feat: add folder with reusable widgets

---
+++ lib/widgets/item_card.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ItemCard extends StatelessWidget {
+  const ItemCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
