6b5e1f1db54a22dac83fe3acaca0c7a6f691af8f
Roy Palomino <roy100995@gmail.com>
2022-08-12 11:22:52 -0500

    refactor: define function handler as required param

--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -1,60 +1,73 @@
 import 'package:flutter/material.dart';
 
 class ItemCard extends StatelessWidget {
-  const ItemCard({ super.key, required this.title, required this.description, required this.icon });
+  const ItemCard({
+    Key? key,
+    required this.title,
+    required this.description,
+    required this.icon,
+    required this.cardHandler,
+  }) : super(key: key);
 
   final String title;
   final String description;
   final IconData icon;
+  final Function cardHandler;
 
   @override
   Widget build(BuildContext context) {
     return OrientationBuilder(builder: (context, orientation) {
-      return Container(
-        margin: const EdgeInsets.only(bottom: 20.0),
-        padding: const EdgeInsets.fromLTRB(12.0, 24.0, 2.0, 24.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8.0),
-          border: Border.all(
-            width: 3.0,
-            color: Colors.black87,
+      return GestureDetector(
+        onTap: () => cardHandler(title),
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 20.0),
+          padding: const EdgeInsets.fromLTRB(12.0, 24.0, 2.0, 24.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            border: Border.all(
+              width: 3.0,
+              color: Colors.black87,
+            ),
           ),
-        ),
-        child: Row(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(right: 12.0),
-              child: Icon(icon, size: 32.0,),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 17.5,
-                  ),
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Icon(
+                  icon,
+                  size: 32.0,
                 ),
-                const SizedBox(height: 8),
-                SizedBox(
-                  width: 260.0,
-                  child: Text(
-                    description,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14.0,
-                      color: Colors.grey.shade500,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 17.5,
                     ),
-                    maxLines: 3,
-                    textDirection: TextDirection.ltr,
-                    overflow: TextOverflow.clip,
-                    textAlign: TextAlign.left,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    width: 260.0,
+                    child: Text(
+                      description,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14.0,
+                        color: Colors.grey.shade500,
+                      ),
+                      maxLines: 3,
+                      textDirection: TextDirection.ltr,
+                      overflow: TextOverflow.clip,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       );
     });
