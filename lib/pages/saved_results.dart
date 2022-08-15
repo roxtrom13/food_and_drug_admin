f4f1ea91ef133de9ff6d94c3cfc308025f1218cd
Roy Palomino <roy100995@gmail.com>
2022-08-15 11:48:39 -0500

    feat: add mock tab to show saved results

---
+++ lib/pages/saved_results.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SavedResultsTab extends StatefulWidget {
+  const SavedResultsTab({Key? key}) : super(key: key);
+
+  @override
+  State<SavedResultsTab> createState() => _SavedResultsTabState();
+}
+
+class _SavedResultsTabState extends State<SavedResultsTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  SizedBox(height: 52.0),
+                  Text(
+                    "FDA App",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            const Text(
+              "Resultados guardados",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 15.0),
+            const Text("Listas guardadas"),
+            const SizedBox(height: 20.0),
+            const Text("List array here"),
+          ],
+        ),
+      ),
+    );
+  }
+}
