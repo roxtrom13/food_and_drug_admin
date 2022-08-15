e48a4ec424a53e79cc1853a7f77f61768aa0dc18
Roy Palomino <roy100995@gmail.com>
2022-08-15 12:35:02 -0500

    refactor: implementing component importing and listing

--- lib/pages/saved_results_tab.dart
+++ lib/pages/saved_results_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_and_drug_admin/widgets/ingredient_list_card.dart';
 
 class SavedResultsTab extends StatefulWidget {
   const SavedResultsTab({Key? key}) : super(key: key);
@@ -13,9 +14,41 @@ class _SavedResultsTabState extends State<SavedResultsTab> {
     return Scaffold(
       body: SafeArea(
         child: ListView(
-          children: const [
-            Text("Hola mundo"),
-            Text("Mundo hola"),
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  SizedBox(height: 52.0),
+                  Text(
+                    "FDA App",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            const Text(
+              "Resultados guardados",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 15.0),
+            const Text("Listas guardadas"),
+            const SizedBox(height: 20.0),
+            const Text("List array here"),
+            const SizedBox(height: 20.0),
+            const IngredientListCard(listTitle: "Lista 1"),
+            const IngredientListCard(listTitle: "Lista 2"),
+            const IngredientListCard(listTitle: "Lista 3"),
           ],
         ),
       ),
