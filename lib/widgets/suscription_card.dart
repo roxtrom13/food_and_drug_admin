1a04b2660e06a121c46d95e5a6ca56497151461d
Roy Palomino <roy100995@gmail.com>
2022-08-17 11:17:43 -0500

    feat: add base widget to display suscription info

---
+++ lib/widgets/suscription_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SuscriptionCard extends StatelessWidget {
+  const SuscriptionCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey.shade200,
+      child: const Text("Suscription card here :)"),
+    );
+  }
+}
