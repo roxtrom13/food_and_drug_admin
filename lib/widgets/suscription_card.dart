41dbaee7c805972da97d15ebfc2e105c3ef59bbd
Roy Palomino <roy100995@gmail.com>
2022-08-17 12:09:36 -0500

    feat: add widget to display credit card info

--- lib/widgets/suscription_card.dart
+++ lib/widgets/suscription_card.dart
@@ -1,13 +1,96 @@
 import 'package:flutter/material.dart';
 
 class SuscriptionCard extends StatelessWidget {
-  const SuscriptionCard({Key? key}) : super(key: key);
+  const SuscriptionCard({
+    Key? key,
+    required this.name,
+    required this.expiryDate,
+    required this.amount,
+  }) : super(key: key);
+
+  final String name;
+  final String expiryDate;
+  final double amount;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey.shade200,
-      child: const Text("Suscription card here :)"),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4.0),
+        color: Colors.grey.shade200,
+      ),
+
+      margin: const EdgeInsets.all(2.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            name,
+            textAlign: TextAlign.start,
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 16.0,
+            ),
+          ),
+          const SizedBox(height: 8.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Fecha de renovación",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey.shade600,
+                ),
+              ),
+              Text(
+                expiryDate,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "\$$amount mensual",
+                textAlign: TextAlign.end,
+                style: const TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.grey.shade400,
+                    fixedSize: const Size(110.0, 10.0)),
+                child: const Text(
+                  "Cancelar",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.purple.shade900,
+                    fixedSize: const Size(110.0, 10.0)),
+                child: const Text("Renovar"),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
