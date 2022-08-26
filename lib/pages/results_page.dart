48f936ff972afa877f4a56ad75041c0a9b32184a
Roy Palomino <roy100995@gmail.com>
2022-08-26 11:59:32 -0500

    feat: add new page to show results by image

---
+++ lib/pages/results_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatefulWidget {
+  const ResultsPage({Key? key}) : super(key: key);
+
+  @override
+  State<ResultsPage> createState() => _ResultsPageState();
+}
+
+class _ResultsPageState extends State<ResultsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text("Resultados"),
+              Icon(Icons.bookmark),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
