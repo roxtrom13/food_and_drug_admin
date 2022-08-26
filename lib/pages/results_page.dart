d572a7a85ceb2f63819afe96f9431a034f9cc8b4
Roy Palomino <roy100995@gmail.com>
2022-08-26 12:14:26 -0500

    feat: implementing common app bar

--- lib/pages/results_page.dart
+++ lib/pages/results_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_and_drug_admin/widgets/common/app_bar.dart';
 
 class ResultsPage extends StatefulWidget {
   const ResultsPage({Key? key}) : super(key: key);
@@ -11,7 +12,9 @@ class _ResultsPageState extends State<ResultsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: genericAppBar(context, "Resultados"),
       body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -24,4 +27,5 @@ class _ResultsPageState extends State<ResultsPage> {
       ),
     );
   }
+
 }
