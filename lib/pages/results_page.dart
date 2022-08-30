e4c1a5e281b77d03d2073d19b9dde849fdd1180f
Roy Palomino <roy100995@gmail.com>
2022-08-30 12:04:53 -0500

    refactor: implement ingredient card to results page

--- lib/pages/results_page.dart
+++ lib/pages/results_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_and_drug_admin/widgets/common/app_bar.dart';
+import 'package:food_and_drug_admin/widgets/ingredient_card.dart';
 
 class ResultsPage extends StatefulWidget {
   const ResultsPage({Key? key}) : super(key: key);
@@ -18,14 +19,64 @@ class _ResultsPageState extends State<ResultsPage> {
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              Text("Resultados"),
-              Icon(Icons.bookmark),
+            children: [
+              const Text("FDA App"),
+              Row(
+                children: const [
+                  Text("Guardar lista"),
+                  Icon(
+                    Icons.bookmark,
+                    semanticLabel: "Guardar lista",
+                  ),
+                ],
+              ),
             ],
-          )
+          ),
+          Text(
+            "Resumen general del producto evaluado",
+            style: TextStyle(
+              color: Colors.grey.shade600,
+            ),
+          ),
+          const SizedBox(height: 6.0),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Ver resumen general"),
+          ),
+          const SizedBox(height: 22.0),
+          Text(
+            "Resultados por ingrediente",
+            style: TextStyle(
+              color: Colors.grey.shade600,
+            ),
+          ),
+          const SizedBox(height: 6.0),
+          Wrap(
+            runSpacing: 20.0,
+            children: const [
+              IngredientCard(
+                name: "Resultado ga",
+                description:
+                    "Laboriosam qui provident nam et adipisci quibusdam. Velit dolorem expedita",
+                damage: 0.3,
+              ),
+              IngredientCard(
+                name: "Resultado ga",
+                description:
+                    "Laboriosam qui provident nam et adipisci quibusdam. Velit dolorem expedita",
+                damage: 0.3,
+              ),
+              IngredientCard(
+                name: "Resultado ga",
+                description:
+                    "Laboriosam qui provident nam et adipisci quibusdam. Velit dolorem expedita",
+                damage: 0.3,
+              ),
+            ],
+          ),
+          const SizedBox(height: 20.0),
         ],
       ),
     );
   }
-
 }
