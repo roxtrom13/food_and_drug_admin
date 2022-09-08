3478254fc7008ccb69ccff6d9d00882201e40e34
Roy Palomino <roy100995@gmail.com>
2022-09-08 17:22:34 -0500

    feat: add new tab to use camera

---
+++ lib/widgets/camera_tab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CameraTab extends StatefulWidget {
+  const CameraTab({Key? key}) : super(key: key);
+
+  @override
+  State<CameraTab> createState() => _CameraTabState();
+}
+
+class _CameraTabState extends State<CameraTab> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
