f52c08b7b97b21460fa31e45d1374023eced8c7e
Roy Palomino <roy100995@gmail.com>
2022-09-08 12:16:10 -0500

    refactor: add styles and basic template

--- lib/pages/search_ingredient_tab.dart
+++ lib/pages/search_ingredient_tab.dart
@@ -11,53 +11,107 @@ class SearchIngredientsTab extends StatefulWidget {
 class _SearchIngredientsTabState extends State<SearchIngredientsTab> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Replace this container with your Map widget
-        Column(
-          children: [
-            Container(
-              height: 200.0,
-              color: Colors.blue,
+    return SingleChildScrollView(
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 200.0,
+                color: Colors.blue,
+              ),
+              const SizedBox(height: 40.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Resultados para \$nombre"),
+                    const SizedBox(height: 20.0),
+                    Wrap(
+                      runSpacing: 12.0,
+                      children: const [
+                        IngredientCard(
+                          name: "Ingrediente 1",
+                          description:
+                              "tnahoe ustnaohe utnhaosnetuhao ntehusn taoheu ntaohe usntoheut s",
+                          damage: 0.2,
+                        ),
+                        IngredientCard(
+                          name: "Ingrediente 2",
+                          description:
+                              "tnahoe ustnaohe utnhaosnetuhao ntehusn taoheu ntaohe usntoheut s",
+                          damage: 0.2,
+                        ),
+                        IngredientCard(
+                          name: "Ingrediente 3",
+                          description:
+                              "tnahoe ustnaohe utnhaosnetuhao ntehusn taoheu ntaohe usntoheut s",
+                          damage: 0.8,
+                        ),
+                        IngredientCard(
+                          name: "Ingrediente 4",
+                          description:
+                              "tnahoe ustnaohe utnhaosnetuhao ntehusn taoheu ntaohe usntoheut s",
+                          damage: 0.2,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SearchWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchWidget extends StatelessWidget {
+  const SearchWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 174,
+      right: 30,
+      left: 30,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            width: 1.0,
+            color: Colors.grey,
+          ),
+        ),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              splashColor: Colors.grey,
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+            const Expanded(
+              child: TextField(
+                cursorColor: Colors.black,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.go,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                    hintText: "Buscar ingredientes"),
+              ),
             ),
           ],
         ),
-        Positioned(
-          top: 10,
-          right: 15,
-          left: 15,
-          child: Container(
-            color: Colors.white,
-            child: Row(
-              children: <Widget>[
-                IconButton(
-                  splashColor: Colors.grey,
-                  icon: Icon(Icons.menu),
-                  onPressed: () {},
-                ),
-                Expanded(
-                  child: TextField(
-                    cursorColor: Colors.black,
-                    keyboardType: TextInputType.text,
-                    textInputAction: TextInputAction.go,
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
-                        hintText: "Search..."),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.deepPurple,
-                    child: Text('RD'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
