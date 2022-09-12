4ed2c5a348392ac23751a536289bb744415e05b8
Roy Palomino <roy100995@gmail.com>
2022-09-12 18:04:38 -0500

    feat: using camera and add base styling
    - Fix: underetmined camera initializations
    - change ratio to full screen camera

--- lib/widgets/camera_tab.dart
+++ lib/widgets/camera_tab.dart
@@ -20,44 +20,123 @@ class _CameraTabState extends State<CameraTab> {
   late List<CameraDescription> _cameras;
   late CameraController _cameraController;
 
-  initializeCamera() async {
-    _widgetState = WidgetState.loading;
-
-    if (mounted) {
-      setState(() {});
-    }
-
-    _cameras = await availableCameras();
-    _cameraController = CameraController(_cameras[0], ResolutionPreset.high);
-
-    await _cameraController.initialize();
-
-    if (_cameraController.value.hasError) {
-      _widgetState = WidgetState.error;
-    } else {
-      _widgetState = WidgetState.loaded;
-    }
-    if (mounted) {
-      setState(() {});
-    }
-  }
-
   @override
   void initState() {
     super.initState();
-
     initializeCamera();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    initializeCamera();
+    switch (_widgetState) {
+      case WidgetState.none:
+      case WidgetState.loading:
+        return _buildScaffold(
+          context,
+          const Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      case WidgetState.loaded:
+        return Stack(
+          children: [
+            Positioned.fill(
+              child: CameraPreview(_cameraController),
+            ),
+            Positioned.fill(
+              child: Scaffold(
+                backgroundColor: Colors.transparent,
+                appBar: AppBar(backgroundColor: Colors.transparent),
+                body: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 50,
+                      right: 20,
+                      left: 20,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: 40.0,
+                            width: 40.0,
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              border: Border.all(
+                                color: Colors.black,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(5),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            iconSize: 60.0,
+                            icon: const Icon(
+                              Icons.camera,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                          const SizedBox(
+                            width: 40,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      case WidgetState.error:
+        return _buildScaffold(
+          context,
+          const Center(
+            child: Text("La cámara no se pudo inicializar."),
+          ),
+        );
+    }
+  }
 
-    // switch(_widgetState) {
-    //   case WidgetState.none:
-    //     jj
-    // }
+  Widget _buildScaffold(BuildContext context, Widget body) {
+    // return Container(
+    //   child: body,
+    // backgroundColor: Colors.black,
+    // body: body,
+    // floatingActionButton: FloatingActionButton(
+    //   onPressed: () {},
+    //   child: const Icon(Icons.camera),
+    // ),
+    // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    // );
+    return body;
+  }
 
-    return const Text("Hola mundo");
+  Future<void> initializeCamera() async {
+    _widgetState = WidgetState.loading;
+
+    if (mounted) setState(() {});
+
+    _cameras = await availableCameras();
+    _cameraController = CameraController(_cameras[0], ResolutionPreset.medium);
+
+    await _cameraController.initialize();
+
+    if (_cameraController.value.hasError) {
+      print("Has error :(");
+      _widgetState = WidgetState.error;
+      if (mounted) setState(() {});
+    } else {
+      print("All good :)");
+      _widgetState = WidgetState.loaded;
+      if (mounted) setState(() {});
+    }
   }
 }
