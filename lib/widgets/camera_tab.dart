319e60c8b0e62051ebce95326308a18cd7ad0da3
Roy Palomino <roy100995@gmail.com>
2022-09-12 16:10:10 -0500

    feat: add iniliazire for camera

--- lib/widgets/camera_tab.dart
+++ lib/widgets/camera_tab.dart
@@ -8,10 +8,56 @@ class CameraTab extends StatefulWidget {
   State<CameraTab> createState() => _CameraTabState();
 }
 
+enum WidgetState {
+  none,
+  loading,
+  loaded,
+  error,
+}
+
 class _CameraTabState extends State<CameraTab> {
+  WidgetState _widgetState = WidgetState.none;
+  late List<CameraDescription> _cameras;
+  late CameraController _cameraController;
+
+  initializeCamera() async {
+    _widgetState = WidgetState.loading;
+
+    if (mounted) {
+      setState(() {});
+    }
+
+    _cameras = await availableCameras();
+    _cameraController = CameraController(_cameras[0], ResolutionPreset.high);
+
+    await _cameraController.initialize();
+
+    if (_cameraController.value.hasError) {
+      _widgetState = WidgetState.error;
+    } else {
+      _widgetState = WidgetState.loaded;
+    }
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeCamera();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    initializeCamera();
+
+    // switch(_widgetState) {
+    //   case WidgetState.none:
+    //     jj
+    // }
+
+    return const Text("Hola mundo");
   }
 }
